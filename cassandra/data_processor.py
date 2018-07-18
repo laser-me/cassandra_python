7be52843c15e2f61a65b7cea4b7e31a419c3bad8
Andrey Lukyanenko <laser.me@yandex.ru>
2018-07-18 14:38:52 +0300

    insertion of schema to cassandra

--- cassandra/data_processor.py
+++ cassandra/data_processor.py
@@ -5,11 +5,18 @@ def convert_data():
 
     f.write("CREATE KEYSPACE assignment WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};\n")
     f.write(
-        "CREATE TABLE assignment.logs (player_id: text, country: text, event: text, session_id: UUID, ts: timestamp)\n")
+        """CREATE TABLE assignment.logs ( \
+            player_id text, \
+            country text, \
+            event text, \
+            session_id UUID, \
+            ts timestamp, \
+            PRIMARY KEY (session_id, ts) \
+            );\n""")
 
     with open("./assignment_data.jsonl") as json_lines:
         for line in json_lines:
-            f.write("INSERT INTO assignment.logs JSON '" + line.strip() + "'\n")
+            f.write("INSERT INTO assignment.logs JSON '" + line.strip() + "';\n")
 
     f.close()
 
