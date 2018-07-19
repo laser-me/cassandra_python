83a340f01e5893d3e861c82d208d3c9172f13b37
Andrey Lukyanenko <laser.me@yandex.ru>
2018-07-19 12:00:16 +0300

    new table instead of view (due to limitation of cassandra 3.11)

--- cassandra/data_processor.py
+++ cassandra/data_processor.py
@@ -29,6 +29,7 @@ def convert_data():
 
                 if json_obj['event'] == 'start':
                     cql = "UPDATE a.events SET ts_start='%(ts)s', country='%(country)s' WHERE player_id='%(player_id)s' AND session_id=%(session_id)s;\n" % json_obj
+                    cql += "INSERT INTO a.events_start_by_country JSON '" + json.dumps(json_obj).strip() + "';\n"
 
                 else:
                     cql = "UPDATE a.events SET ts_end='%(ts)s' WHERE player_id='%(player_id)s' AND session_id=%(session_id)s;\n" % json_obj
